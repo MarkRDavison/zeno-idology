2e805de851100cc7b9a132bb0777c2127fafe29b
mark.davison <markdavison0+github@gmail.com>
2026-04-28 13:06:03 +1200

    Add basic staff name scrollable list

--- Conservation/Idology.Conservation.Core/Widgets/StaffDetailsUiSubScenePanelWidget.cs
+++ Conservation/Idology.Conservation.Core/Widgets/StaffDetailsUiSubScenePanelWidget.cs
@@ -2,11 +2,15 @@
 
 public sealed class StaffDetailsUiSubScenePanelWidget : UiSubScenePanelWidget
 {
+    private readonly ConservationGameData _gameData;
+
     public StaffDetailsUiSubScenePanelWidget(
+        ConservationGameData gameData,
         ITranslationService translationService
     ) : base(
         translationService)
     {
+        _gameData = gameData;
     }
 
     public override string TitleTranslationKey => "STAFF_DETAILS_TITLE";
@@ -14,5 +18,37 @@ public sealed class StaffDetailsUiSubScenePanelWidget : UiSubScenePanelWidget
     public override void PostConstructInit()
     {
         var scrollableWidget = AddCommonWidgets();
+
+        foreach (var s in _gameData.StaffData)
+        {
+            var currentPanel = scrollableWidget.AddChild(new PanelWidget
+            {
+                Background = Color.Gray,
+                Border = Color.DarkGray,
+                BorderThickness = 2.0f,
+                Layout =
+                {
+                    RequestedPadding = new LayoutEdges(8.0f),
+                    RequestedMargin = new LayoutEdges(0.0f, 0.0f, 0.0f, 2.0f), // TODO: REPLACE WITH PARENT GAP
+                    Behave = BehaveFlags.HFill,
+                    Contain = ContainFlags.Row,
+                    Align = AlignFlags.Start,
+                    ItemFlags = ItemFlags.VFixed
+                }
+            });
+
+            currentPanel.AddChild(new LabelWidget
+            {
+                Foreground = Color.White,
+                TextContent = s.Name,
+                FontSize = 24,
+                Layout =
+                {
+                    RequestedMargin = new LayoutEdges(4.0f),
+                    Behave = BehaveFlags.VCenter | BehaveFlags.Left,
+                    RequestedSize = new LayoutVector(0, 32)
+                }
+            });
+        }
     }
 }
