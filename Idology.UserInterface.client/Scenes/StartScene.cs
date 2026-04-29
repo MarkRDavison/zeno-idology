a2601f682b2acc8d2e19b9010d41361cb0b78acc
mark.davison <markdavison0+github@gmail.com>
2026-04-29 18:57:10 +1200

    Proof of concept of having sibling UI widgets on top of each other

--- Idology.UserInterface.client/Scenes/StartScene.cs
+++ Idology.UserInterface.client/Scenes/StartScene.cs
@@ -78,40 +78,56 @@ internal sealed class StartScene : Scene<StartScene>
                 {
                     RequestedMargin = new LayoutEdges(32.0f),
                     Behave = BehaveFlags.Fill,
-                    Contain = ContainFlags.Column
+                    Contain = ContainFlags.Row
                 }
             });
 
-            var button = background.AddChild(new TextButtonWidget
             {
-                TextContent = "BUTTON 1",
-                Foreground = Color.Yellow,
-                Background = Color.Green,
-                Border = Color.DarkBlue,
-                BorderThickness = 4.0f,
-                Layout = new LayoutItem
+                var leftPanel = background.AddChild(new PanelWidget
                 {
-                    Behave = BehaveFlags.Center,
-                    RequestedSize = new LayoutVector(256, 96)
-                }
-            });
+                    Background = Color.Gray,
+                    Border = Color.DarkGray,
+                    BorderThickness = 8.0f,
+                    Layout = new LayoutItem
+                    {
+                        RequestedMargin = new LayoutEdges(32.0f),
+                        Behave = BehaveFlags.Fill,
+                        Contain = ContainFlags.Column
+                    }
+                });
 
-            button.OnClick += (s, e) => Console.WriteLine("BUTTON 1 IS CLICKED!");
 
-            var button2 = background.AddChild(new DropdownWidget
-            {
-                Foreground = Color.Yellow,
-                Background = Color.Green,
-                Border = Color.DarkBlue,
-                BorderThickness = 4.0f,
-                Layout = new LayoutItem
+
+                var button = leftPanel.AddChild(new TextButtonWidget
                 {
-                    RequestedMargin = new LayoutEdges(0.0f, 8.0f, 0.0f, 0.0f),
-                    Behave = BehaveFlags.Center,
-                    RequestedSize = new LayoutVector(256, 64)
-                },
-                SelectedItemId = "3",
-                Items =
+                    TextContent = "BUTTON 1",
+                    Foreground = Color.Yellow,
+                    Background = Color.Green,
+                    Border = Color.DarkBlue,
+                    BorderThickness = 4.0f,
+                    Layout = new LayoutItem
+                    {
+                        Behave = BehaveFlags.Center,
+                        RequestedSize = new LayoutVector(256, 96)
+                    }
+                });
+
+                button.OnClick += (s, e) => Console.WriteLine("BUTTON 1 IS CLICKED!");
+
+                var button2 = leftPanel.AddChild(new DropdownWidget
+                {
+                    Foreground = Color.Yellow,
+                    Background = Color.Green,
+                    Border = Color.DarkBlue,
+                    BorderThickness = 4.0f,
+                    Layout = new LayoutItem
+                    {
+                        RequestedMargin = new LayoutEdges(0.0f, 8.0f, 0.0f, 0.0f),
+                        Behave = BehaveFlags.Center,
+                        RequestedSize = new LayoutVector(256, 64)
+                    },
+                    SelectedItemId = "3",
+                    Items =
                 {
                     new DropdownItem("Item 1", "1"),
                     new DropdownItem("Item 2", "2"),
@@ -119,13 +135,60 @@ internal sealed class StartScene : Scene<StartScene>
                     new DropdownItem("Item 4 with more text", "4"),
                     new DropdownItem("Item 5", "5"),
                 }
-            });
+                });
 
-            button2.OnItemSelected += (s, e) =>
+                button2.OnItemSelected += (s, e) =>
+                {
+                    Console.WriteLine("Selected: '{0}' ({1})", e.Text, e.Id);
+                    button2.SelectedItemId = e.Id;
+                };
+            }
+
             {
-                Console.WriteLine("Selected: '{0}' ({1})", e.Text, e.Id);
-                button2.SelectedItemId = e.Id;
-            };
+                var rightPanel = background.AddChild(new PanelWidget
+                {
+                    Background = Color.Gray,
+                    Border = Color.DarkGray,
+                    BorderRoundness = 0.025f,
+                    BorderThickness = 8.0f,
+                    Layout = new LayoutItem
+                    {
+                        RequestedMargin = new LayoutEdges(32.0f),
+                        Behave = BehaveFlags.Fill,
+                        Contain = ContainFlags.Layout
+                    }
+                });
+
+
+                var rightSubPanel1 = rightPanel.AddChild(new PanelWidget
+                {
+                    Background = Color.Green,
+                    Border = Color.DarkGray,
+                    BorderRoundness = 0.025f,
+                    BorderThickness = 8.0f,
+                    Layout = new LayoutItem
+                    {
+                        RequestedMargin = new LayoutEdges(32.0f),
+                        Behave = BehaveFlags.Fill,
+                        Contain = ContainFlags.Column
+                    }
+                });
+
+                var rightSubPanel2 = rightPanel.AddChild(new PanelWidget
+                {
+                    Background = Color.Red,
+                    Border = Color.DarkGray,
+                    BorderRoundness = 0.025f,
+                    BorderThickness = 8.0f,
+                    Layout = new LayoutItem
+                    {
+                        RequestedSize = new LayoutVector(256.0f, 500.0f),
+                        RequestedMargin = new LayoutEdges(64.0f),
+                        Behave = BehaveFlags.Left | BehaveFlags.VCenter,
+                        Contain = ContainFlags.Column
+                    }
+                });
+            }
         }
     }
 
